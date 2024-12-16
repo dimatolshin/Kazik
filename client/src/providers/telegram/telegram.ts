af439d052eeb00f052c80e66bdbf2f1647027dd0
unknown <UstinovichSergey>
2024-12-16 14:32:26 +0300

    /

--- client/src/providers/telegram/telegram.ts
+++ client/src/providers/telegram/telegram.ts
@@ -7,13 +7,13 @@ declare global {
 export const useTelegram = () => {
   const tg = window.Telegram.WebApp;
 
-  const userName = tg?.initDataUnsafe?.user?.username;
-  const tg_id = tg?.initDataUnsafe?.user?.id;
-  const firstName = tg?.initDataUnsafe?.user?.first_name;
+  // const userName = tg?.initDataUnsafe?.user?.username;
+  // const tg_id = tg?.initDataUnsafe?.user?.id;
+  // const firstName = tg?.initDataUnsafe?.user?.first_name;
   const photo = tg?.initDataUnsafe?.user?.photo_url;
-  // const tg_id = "12323123";
-  // const userName = "byngra";
-  // const firstName = 'Сергей';
+  const tg_id = "12323123";
+  const userName = "byngra";
+  const firstName = 'Сергей';
  
   return { tg, userName, tg_id, photo, firstName };
 };
